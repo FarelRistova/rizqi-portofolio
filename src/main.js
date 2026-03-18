58e58b2185b44225e428f669c1be9a1cf862ce20
FarelRistova <farel.ristova@gmail.com>
2026-03-18 14:31:33 +0700

    resolve main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import AOS from 'aos'
+import Typed from 'typed.js'
+import GLightbox from 'glightbox'
+import imagesLoaded from 'imagesloaded'
+import Isotope from 'isotope-layout'
+import 'waypoints/lib/noframework.waypoints.js'
 
 import './assets/css/main.css'
 import './assets/vendor/bootstrap/css/bootstrap.min.css'
@@ -8,17 +13,18 @@ import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
 import './assets/vendor/swiper/swiper-bundle.min.css'
 import './assets/vendor/glightbox/css/glightbox.min.css'
 
-// import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
-// import './assets/vendor/php-email-form/validate.js'
-// import './assets/vendor/typed.js/typed.umd.js'
-// import './assets/vendor/purecounter/purecounter_vanilla.js'
-// import './assets/vendor/waypoints/noframework.waypoints.js'
-// import './assets/vendor/glightbox/js/glightbox.min.js'
-// import './assets/vendor/imagesloaded/imagesloaded.pkgd.min.js'
-// import './assets/vendor/isotope-layout/isotope.pkgd.min.js'
-// import './assets/vendor/swiper/swiper-bundle.min.js'
+import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
+import './assets/vendor/php-email-form/validate.js'
+import './assets/vendor/purecounter/purecounter_vanilla.js'
+import './assets/vendor/swiper/swiper-bundle.min.js'
 
 import 'aos/dist/aos.css'
+
+window.Typed = Typed
+window.GLightbox = GLightbox
+window.imagesLoaded = imagesLoaded
+window.Isotope = Isotope
+
 AOS.init()
 
 const app = createApp(App)
